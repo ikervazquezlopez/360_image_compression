bd26f87a131668cda03a3401dca583aa64620252
Iker Vazquez Lopez <ikervazquezlopez@u.boisestate.edu>
2019-12-03 11:57:19 -0700

    Compression and decompression with command arguments and working correctly.

--- 360_image_compression.py
+++ 360_image_compression.py
@@ -1,38 +1,38 @@
 import cv2
 import numpy as np
-
-
-
-
-pano = cv2.imread("data/test.jpeg")
-
-h, w, c = pano.shape
-
-#canvas = np.zeros_like(pano)
-canvas = np.zeros( (int(h/2), w+1, c), dtype=np.uint8)
+import sys
 
 d = 4 # Increment rate per row
 
+
+pano = cv2.imread(sys.argv[1])
+h, w, c = pano.shape
+
+# Create output image which half height and width+1 to fit both triangles
+canvas = np.zeros( (int(h/2), w-2, c), dtype=np.uint8)
+
+# Compress and fill the northern hemisphere
 for r in range(0, int(h/2)):
-#for r in range(1, 4):
     chunks = 1 + r * d
     chunk_size =  w / chunks
     for c in range(0, chunks):
         c_start = int(c*chunk_size)
         canvas[r,c] = pano[r, c_start]
-        pano[r, int(c_start + chunk_size/2)] = [255, 0, 0]
+
+# Flip horizontally and vertically
+pano = cv2.flip(pano, -1)
+canvas = cv2.flip(canvas, -1)
+
+# Compress and fill the southern hemisphere after flipping
 for r in range(0, int(h/2)):
     chunks = 1 + r * d
     chunk_size =  w / chunks
     for c in range(0, chunks):
         c_start = int(c*chunk_size)
-        canvas[int(h/2)-r-1,w-c-3] = pano[h-r-1, w-c_start-1]
+        #canvas[int(h/2)-r-1,w-c-3] = pano[h-r-1, w-c_start-1]
+        canvas[r,c] = pano[r, c_start]
 
-cv2.imwrite("out.jpeg", canvas)
-#cv2.imwrite("out_pano.png", pano)
+# Flip canvas back
+canvas = cv2.flip(canvas, -1)
 
-img = canvas[:10, :10]
-img = cv2.resize(img, None, fx=100, fy=100, interpolation=cv2.INTER_NEAREST)
-cv2.imshow("out", img)
-cv2.waitKey(0)
-cv2.destrowAllWindows()
+cv2.imwrite(sys.argv[2], canvas)
--- 360_image_decompression.py
+++ 360_image_decompression.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+import sys
 
+d = 4 # Increment rate per row
 
 def row_linear_interpolation(p, r, c, c_start, chunk_size):
     d_start = (p - c_start) / chunk_size
@@ -9,20 +11,18 @@ def row_linear_interpolation(p, r, c, c_start, chunk_size):
     return color.astype(np.uint8)
 
 
-cpano = cv2.imread("out.jpeg")
-
+cpano = cv2.imread(sys.argv[1])
 h, w, c = cpano.shape
 
-#canvas = np.zeros_like(pano)
 canvas = np.zeros( (h*2, w, c), dtype=np.uint8)
 
-d = 4 # Increment rate per row
-
-for c in range(0, w-2): # Set middle row without compression
+# Build middle rows without any interpolation
+for c in range(0, w-2):
     canvas[h-1, c] = cpano[h-1, c]
     canvas[h, c] = cpano[0, c+2]
 
-for r in range(h-2, 0, -1):
+# Reconstruct northern hemisphere
+for r in range(h-1, 0, -1):
     chunks = 1 + r * d
     chunk_size = w / chunks
     for c in range(0, chunks):
@@ -32,11 +32,12 @@ for r in range(h-2, 0, -1):
         for p in range(c_start+1, c_end+1):
             canvas[r, p] = row_linear_interpolation(p, r, c, c_start, int(chunk_size))
 
-
+# Flip horizontally and vertically
 cpano = cv2.flip(cpano, -1)
 canvas = cv2.flip(canvas, -1)
 
-for r in range(h-2, 0, -1):
+# Reconstruct the southern hemisphere
+for r in range(h-1, 0, -1):
     chunks = 1 + r * d
     chunk_size = w / chunks
     for c in range(0, chunks):
@@ -45,18 +46,8 @@ for r in range(h-2, 0, -1):
         canvas[r, c_start] = cpano[r, c]
         for p in range(c_start+1, c_end+1):
             canvas[r, p] = row_linear_interpolation(p, r, c, c_start, int(chunk_size))
-"""
-for r in range(0, h):
-    chunks = 1 + (h-r) * d
-    chunk_size = w / chunks
-    for c in range(w-2, w-chunks, -1):
-        if r == 376:
-            print(r, c, chunks, chunk_size)
-        c_start = w-int(c*chunk_size)
-        c_end = c_start - int(chunk_size)
-        canvas[h+r, c_start] = cpano[r, c]
-        for p in range(c_start, c_end, -1):
-            canvas[h+r, p] = row_linear_interpolation(p, r, c, c_start, int(chunk_size))
-"""
+
+# Flip canvas back
 canvas = cv2.flip(canvas, -1)
-cv2.imwrite("reconstructed.png", canvas)
+
+cv2.imwrite(sys.argv[2], canvas)
